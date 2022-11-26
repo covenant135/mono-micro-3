95bbd8a77b74c1f4c2220bebd387a7845926ca16
SCHULL TECHNOLOGIES <abiodun.olaitan46@gmail.com>
2022-11-26 16:43:43 +0100

    working on using git action

---
+++ .github/workflows/docker_push.sh
@@ -0,0 +1,13 @@
+  shell: /usr/bin/bash -e {0}
+  env:
+    DOCKER_USERNAME: covenantson
+    DOCKER_PASSWORD: 123Apoms$$
+    POSTGRES_USERNAME: olaudagram
+    POSTGRES_PASSWORD: olaudagram
+    POSTGRES_DB: postgres
+    POSTGRES_HOST: laudagram.cdvuedw14gub.us-east-1.rds.amazonaws.com
+    AWS_ACCESS_KEY_ID: ASIAZXA7FJC3AZFIFATF
+    AWS_SECRET_ACCESS_KEY: w4ir1t5vDK1oRAsnkZUfGGgdv5DuFt7SP+7vitHr
+    AWS_REGION: us-east-1
+    AWS_BUCKET:olaudagram 
+    JWT_SECRET: testing
