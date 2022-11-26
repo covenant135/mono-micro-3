ea86919cd06211b5c670ad966fa95c905e79fcae
SCHULL TECHNOLOGIES <abiodun.olaitan46@gmail.com>
2022-11-26 20:59:10 +0100

    new git workflow v1

--- docker_push.sh
+++ docker_push.sh
@@ -2,14 +2,14 @@
 
 docker-compose -f docker-compose-build.yaml build
 
+
+
+docker tag udagram-reverseproxy "covenantson/udagram-reverseproxy:"
+docker tag udagram-api-user "covenantson/udagram-api-user"
+docker tag udagram-api-feed "covenantson/udagram-api-feed:"
+docker tag udagram-frontend:local "covenantson/udagram-frontend:local"
+
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
-
-docker tag udagram-reverseproxy "${DOCKER_USERNAME}/udagram-reverseproxy:"
-docker tag udagram-api-user "${DOCKER_USERNAME}/udagram-api-user"
-docker tag udagram-api-feed "${DOCKER_USERNAME}/udagram-api-feed:"
-docker tag udagram-frontend:local "${DOCKER_USERNAME}/udagram-frontend:local"
-
-
 
 docker push "${DOCKER_USERNAME}/udagram-reverseproxy" &
 docker push "${DOCKER_USERNAME}/udagram-api-user" &
