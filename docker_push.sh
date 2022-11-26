aefe6440ff2e3cd93592b667944b3c187333728e
SCHULL TECHNOLOGIES <abiodun.olaitan46@gmail.com>
2022-11-26 19:47:43 +0100

    working on using git action v3

--- docker_push.sh
+++ docker_push.sh
@@ -5,12 +5,12 @@ docker-compose -f docker-compose-build.yaml build
 docker tag reverseproxy "${DOCKER_USERNAME}/udagram-reverseproxy:v1"
 docker tag udagram-api-user "${DOCKER_USERNAME}/udagram-api-user:v1"
 docker tag udagram-api-feed "${DOCKER_USERNAME}/udagram-api-feed:v1"
-docker tag udagram-frontend:local "${DOCKER_USERNAME}/udagram-frontend:latest"
+docker tag udagram-frontend:local "${DOCKER_USERNAME}/udagram-frontend:local"
 
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
 
 docker push "${DOCKER_USERNAME}/udagram-reverseproxy:v1" &
 docker push "${DOCKER_USERNAME}/udagram-api-user:v1" &
 docker push "${DOCKER_USERNAME}/udagram-api-feed:v1" &
-docker push "${DOCKER_USERNAME}/udagram-frontend:latest" &
+docker push "${DOCKER_USERNAME}/udagram-frontend:local" &
 wait
