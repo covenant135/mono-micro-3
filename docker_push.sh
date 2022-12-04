cbaa45cfb9a459a2d6bb190302ee1091a3a69d49
ABIODUN OLAITAN <abiodun.olaitan46@gmail.com>
2022-12-04 04:04:28 -0500

    updated docker images tags v2

--- docker_push.sh
+++ docker_push.sh
@@ -10,8 +10,8 @@ docker tag udagram-api-feed:v1 "covenantson/udagram-api-feed:v2.1"
 docker tag udagram-frontend:local "covenantson/udagram-frontend:local"
 
 
-docker push "${DOCKER_USERNAME}/udagram-reverseproxy:v2.1" &
-docker push "${DOCKER_USERNAME}/udagram-api-user:v2.1" &
-docker push "${DOCKER_USERNAME}/udagram-api-feed:v2.1" &
-docker push "${DOCKER_USERNAME}/udagram-frontend:local" &
+docker push "covenantson/udagram-reverseproxy:v2.1" &
+docker push "covenantson/udagram-api-user:v2.1" &
+docker push "covenantson/udagram-api-feed:v2.1" &
+docker push "covenantson/udagram-frontend:local" &
 wait
