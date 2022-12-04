788c518b7c925234b27239623675a2aef571e577
ABIODUN OLAITAN <abiodun.olaitan46@gmail.com>
2022-12-04 04:58:55 -0500

    working on k8s

--- docker_push.sh
+++ docker_push.sh
@@ -5,13 +5,13 @@ echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-
 docker-compose -f docker-compose-build.yaml build
 
 docker tag udagram-reverseproxy "covenantson/udareverseproxy:v2.1"
-docker tag udagram-api-user "covenantson/udaapi-user:v2.1"
-docker tag udagram-api-feed "covenantson/udaapi-feed:v2.1"
+docker tag udagram-api-user "covenantson/udaapi-user:v2.5"
+docker tag udagram-api-feed "covenantson/udaapi-feed:v2.5"
 docker tag udagram-frontend "covenantson/udafrontend:local"
 
 
 docker push "covenantson/udareverseproxy:v2.1" &
-docker push "covenantson/udaapi-user:v2.1" &
-docker push "covenantson/udaapi-feed:v2.1" &
+docker push "covenantson/udaapi-user:v2.5" &
+docker push "covenantson/udaapi-feed:v2.5" &
 docker push "covenantson/udafrontend:local" &
 wait
