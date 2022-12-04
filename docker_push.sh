92c16f61054a4f67df6e09a26340190b4d1d0604
ABIODUN OLAITAN <abiodun.olaitan46@gmail.com>
2022-12-04 03:37:05 -0500

    updated docker images tags

--- docker_push.sh
+++ docker_push.sh
@@ -4,14 +4,14 @@ echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-
 
 docker-compose -f docker-compose-build.yaml build
 
-docker tag udagram-reverseproxy:v1 "covenantson/udagram-reverseproxy:v1"
-docker tag udagram-api-user:v1 "covenantson/udagram-api-user:v1"
-docker tag udagram-api-feed:v1 "covenantson/udagram-api-feed:v1"
+docker tag udagram-reverseproxy:v1 "covenantson/udagram-reverseproxy:v2.1"
+docker tag udagram-api-user:v1 "covenantson/udagram-api-user:v2.1"
+docker tag udagram-api-feed:v1 "covenantson/udagram-api-feed:v2.1"
 docker tag udagram-frontend:local "covenantson/udagram-frontend:local"
 
 
-docker push "${DOCKER_USERNAME}/udagram-reverseproxy:v1" &
-docker push "${DOCKER_USERNAME}/udagram-api-user:v1" &
-docker push "${DOCKER_USERNAME}/udagram-api-feed:v1" &
+docker push "${DOCKER_USERNAME}/udagram-reverseproxy:v2.1" &
+docker push "${DOCKER_USERNAME}/udagram-api-user:v2.1" &
+docker push "${DOCKER_USERNAME}/udagram-api-feed:v2.1" &
 docker push "${DOCKER_USERNAME}/udagram-frontend:local" &
 wait
